00f2f1377136e06e6b15e8f7ccc030a752bfdb4d
Maria Camila Cortes Niño <mariac.cortesn@unilibre.edu.co>
2019-10-16 21:30:34 -0500

    Hola bebes

---
+++ src/main/java/com/example/Parcial_Eureka/ParcialEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Parcial_Eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Parcial_Eureka/ParcialEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Parcial_Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
